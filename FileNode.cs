3f5c57c021cc666a050d913e899dc6d070020192
tp <theotime.perras@gmail.com>
2025-12-15 13:46:56 -0500

    Initial commit. Parsed search query result into tree structure.

---
+++ FileNode.cs
@@ -0,0 +1,112 @@
+using Soulseek;
+using static SimpleSeek.Program;
+
+namespace SimpleSeek;
+
+class File {
+    public enum Kind : byte {
+        Regular,
+        Directory
+    }
+
+    public string name;
+    public Kind kind;
+
+    public File(string name) {
+        this.name = name;
+    }
+
+    public virtual string ToString(int level) => $"{String.Concat(Enumerable.Repeat('\t', level))}{name}\n";
+    public override string ToString() => name;
+}
+
+class Directory : File {
+    public List<File> children;
+
+    public Directory(string name, int cap = 1) : base(name) {
+        kind = File.Kind.Directory;
+        children = new(cap);
+    }
+
+    public Directory(SearchResponse resp) : base(resp.Username) {
+        kind = File.Kind.Directory;
+        children = new();
+        CreateTree(resp.Files, this);
+    }
+
+
+
+    public override string ToString(int level) {
+        StringWriter writer = new();
+        writer.Write(base.ToString(level++));
+        foreach (var child in children)
+            writer.Write(child.ToString(level));
+
+        writer.Close();
+        string ret = writer.ToString();
+        return ret;
+    }
+    public override string ToString() => ToString(0);
+
+    private static void FinalizeDir(List<Directory> loc, int depth) {
+        for (int d = depth; d < loc.Count - 1; ++d) {
+            Directory current = loc[d];
+            if (current.children.Count != 1) {
+                current.children.TrimExcess();
+                continue;
+            };
+
+            Directory child = (Directory)current.children[0];
+            // string += forces more heap allocations than necessary. We could use a StringBuilder for the name (but we're heavily network bound anyways)
+            current.name += child.name;
+            current.children = child.children;
+            loc[d + 1] = current;
+        }
+
+        loc[^1].children.TrimExcess();
+        loc.RemoveRange(depth, loc.Count - depth);
+    }
+
+    public static void CreateTree(IReadOnlyCollection<Soulseek.File> files, Directory root) {
+        List<Directory> loc = new(8) {root};
+
+        int start = 0;
+        string name = files.ElementAt(0).Filename;
+        for (int c = IndexOfFrom(name, 0); c < name.Length; c = IndexOfFrom(name, c)) {
+            Directory dir = new(name.Substring(start, c - start));
+            loc.Last().children.Add(dir);
+            loc.Add(dir);
+            start = c;
+        }
+        loc.Last().children.Add(new(name.Substring(start, name.Length - start)));
+
+        for (int i = 1; i < files.Count; ++i) {
+            name = files.ElementAt(i).Filename;
+            int c = 0;
+            for (int depth = 1; depth < loc.Count; ++depth) {
+                start = c;
+                c = IndexOfFrom(name, c);
+
+                if (!loc[depth].name.StartsWith(name.Substring(start, c - start))) {
+                    FinalizeDir(loc, depth);
+                    goto newloc;
+                }
+            }
+
+            start = c;
+            c = IndexOfFrom(name, c);
+
+            newloc:
+            while (c < name.Length) {
+                Directory dir = new(name.Substring(start, c - start));
+                loc.Last().children.Add(dir);
+                loc.Add(dir);
+                start = c;
+                c = IndexOfFrom(name, start);
+            }
+            loc.Last().children.Add(new(name.Substring(start, name.Length - start)));
+        }
+
+        FinalizeDir(loc, 1);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using Soulseek;
+
+namespace SimpleSeek;
+
+struct UserInfo {
+    public string user;
+    public string pwd;
+}
+
+static class Program {
+    static UserInfo userInfo;
+
+    public static int IndexOfFrom(ReadOnlySpan<char> s, int i, char c = '\\') {
+        do if (s[i++] == c) return i;
+        while (i < s.Length);
+        return s.Length;
+    }
+
+    static SearchResponse[]? FromCache(string file) {
+        if (!System.IO.File.Exists(file))
+            return null;
+
+        BinaryReader reader = new(System.IO.File.Open(file, FileMode.Open));
+        SearchResponse[] resps = new SearchResponse[reader.ReadInt32()];
+        for (int i = 0; i < resps.Length; ++i) {
+            string user = reader.ReadString();
+            Soulseek.File[] files = new Soulseek.File[reader.ReadInt32()];
+            for (int f = 0; f < files.Length; ++f)
+                files[f] = new Soulseek.File(0, reader.ReadString(), 0, "");
+
+            resps[i] = new(user, 0, false, 0, 0, files);
+        }
+
+        reader.Close();
+        return resps;
+    }
+
+    static void CacheResps(SearchResponse[] resps, int size, string file) {
+        BinaryWriter writer = new(System.IO.File.Open(file, FileMode.OpenOrCreate));
+        writer.Write(size);
+        for (int i = 0; i < size; ++i) {
+            var r = resps[i];
+            writer.Write(r.Username);
+            writer.Write(r.Files.Count);
+            foreach(var f in r.Files) {
+                writer.Write(f.Filename);
+            }
+        }
+
+        writer.Close();
+    }
+
+    static int Main(string[] args) {
+        string cache = $"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}/simpleseek"; // for reproducible tests
+        SearchResponse[]? resps = FromCache(cache);
+        if (resps == null) {
+            userInfo.user = Environment.GetEnvironmentVariable("SOULSEEK_USER");
+            userInfo.pwd = Environment.GetEnvironmentVariable("SOULSEEK_PASSWORD");
+
+            SoulseekClient client = new();
+            Task conn = client.ConnectAsync(userInfo.user, userInfo.pwd);
+            conn.Wait();
+
+            resps = new SearchResponse[256];
+            Task search = client.SearchAsync(new("Evoken"), options: new(searchTimeout: 2000, responseLimit: resps.Length));
+
+            int i = -1;
+            client.SearchResponseReceived += (object? sender, SearchResponseReceivedEventArgs ev) => {
+                Interlocked.Increment(ref i);
+                resps[i] = (ev.Response);
+            };
+            search.Wait();
+
+            try {
+                CacheResps(resps, i, cache);
+            } catch (Exception e) {
+                Console.WriteLine($"failed to cache responses: {e.Message}");
+                System.IO.File.Delete(cache);
+            }
+        }
+
+        List<File> files = new(resps.Length);
+        foreach (var r in resps)
+            try {
+                if (r.FileCount > 0) files.Add(new Directory(r));
+            } catch(Exception e) {
+                Console.WriteLine($"Threw while processing response from {r.Username}. {e.Message}:\n {e.StackTrace}");
+            }
+        foreach (var f in files)
+            Console.WriteLine(f);
+        return 0;
+    }
+}
